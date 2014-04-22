c5f2dd209f8e4483c03c90cf3bdf28f3c2953d67
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2014-04-22 12:45:41 +0200

    extended test script, use more peers in setup

--- test.py
+++ test.py
@@ -1,38 +1,53 @@
 #!/usr/bin/python3
+# you must install pexpect for this to work:
+#	sudo apt-get install python3-pip
+#	sudo pip3 install pexpect
 import time
 import pexpect
 
-setup = pexpect.spawn('./setup.py')
-herp = pexpect.spawn('./interactive_peer.py localhost 8200 herp 5')
-derp = pexpect.spawn('./interactive_peer.py localhost 8201 derp 5')
+try:
+	setup = pexpect.spawn('./setup.py', timeout=5)
+	herp = pexpect.spawn('./interactive_peer.py localhost 8200 herp 5', timeout=5)
+	derp = pexpect.spawn('./interactive_peer.py localhost 8201 derp 5', timeout=5)
 
-#setup herp
-herp.expect('>')
-herp.sendline('secret private.pem')
-herp.expect('>')
-herp.sendline('hello')
-herp.expect('>')
+	#setup herp
+	herp.expect('>')
+	herp.sendline('secret private.pem')
+	herp.expect('>')
+	herp.sendline('hello')
+	herp.expect('>')
 
-#setup derp
-derp.expect('>')
-derp.sendline('secret private2.pem')
-derp.expect('>')
-derp.sendline('hello')
-derp.expect('>')
+	#setup derp
+	derp.expect('>')
+	derp.sendline('secret private2.pem')
+	derp.expect('>')
+	derp.sendline('hello')
+	derp.expect('>')
 
-# wait for network to manifest
-time.sleep(2)
+	# wait for network to manifest
+	time.sleep(2)
 
-# befriend eachother
-herp.sendline('friend derp public2.pem')
-herp.expect('>')
-derp.sendline('friend herp public.pem')
-derp.expect('>')
+	# befriend eachother
+	herp.sendline('friend derp public2.pem')
+	herp.expect('>')
+	derp.sendline('friend herp public.pem')
+	derp.expect('>')
 
-#send message
-msg = 'plplplplplp'
-herp.sendline('message derp '+msg)
-derp.expect('Received Message from herp: '+msg+'\r\n')
-herp.expect('derp received message! \(verified\)\r\n')
+	#send message via flooding
+	msg = 'plplplplplp'
+	herp.sendline('message derp '+msg)
+	derp.expect('Received Message from herp: '+msg+'\r\n')
+	herp.expect('derp received message! \(verified\)\r\n')
+	print('Flooding message success!')
 
-print('Success!')
+	# send message via k walker
+	herp.sendline('kmessage derp '+msg)
+	derp.expect('Received Message from herp: '+msg+'\r\n')
+	herp.expect('Message delivered and acknowledged:.+\r\n')
+	print('KWalker message success!')
+except pexpect.TIMEOUT as err:
+	print('herp.before: '+str(herp.before))
+	print('herp.after: '+str(herp.after))
+	print('derp.before: '+str(derp.before))
+	print('derp.after: '+str(derp.after))
+	raise err
