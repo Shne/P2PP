407230ee0d0ca81e1a36e3ce7b1c862fc054ba04
Roland42 <thealeran@gmail.com>
2014-05-26 11:39:57 -0700

    PoW test

--- test_proofOfWorkJoiningTime.py
+++ test_proofOfWorkJoiningTime.py
@@ -4,22 +4,24 @@
 #	sudo pip3 install pexpect
 import time
 import pexpect
-import re
 from peer import Peer
 import random
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Test Script')
+parser.add_argument('-peers', type=int, help='Number of peers to set up', default=20)
+args = parser.parse_args()
 
 try:
     #print('Setting up network...')
     #setup = pexpect.spawn('python3.4 setup.py -peers 100', timeout=2000)
     #setup.expect('>')
     start = time.time()
-    print('start: {:.2f}'.format(start))
-
-    numberOfPeers = 20
+    numberOfPeers = args.peers
     names = ['Peer'+str(x) for x in range(numberOfPeers)]
     IP = 'localhost'
     ports = [7300+x for x in range(numberOfPeers)]
-
 
     for (i,name) in enumerate(names):
         limit = 3
@@ -36,6 +38,12 @@ try:
     end = time.time()
     totalTime = '{:.2f}'.format(end-start)
     print(totalTime + ' sec to join network for ' + str(numberOfPeers) + ' peers')
+
+    with open("proofOfWOrkJoiningTimeTestData.txt", "a") as testfile:
+        testfile.write(str(numberOfPeers) + ", " + str(totalTime) + "\n")
+        testfile.close()
+
+
 
 except pexpect.TIMEOUT as err:
     print('Timeout error')
